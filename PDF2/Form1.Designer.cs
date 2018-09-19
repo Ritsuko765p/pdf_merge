45798247ce641a2a98e23bbb1ce3d8b54ee53527
Ritsuko765p <ritsuko765p@live.jp>
2018-09-19 22:35:53 +0900

    1

---
+++ PDF2/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace PDF2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.path = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(175, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "上へ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(175, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "下へ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(175, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "結合";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.path});
+            this.dataGridView1.Location = new System.Drawing.Point(9, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(160, 101);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            this.dataGridView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragEnter);
+            // 
+            // name
+            // 
+            this.name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.name.HeaderText = "name";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // path
+            // 
+            this.path.HeaderText = "path";
+            this.path.Name = "path";
+            this.path.ReadOnly = true;
+            this.path.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 123);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "PDF結合";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn path;
+    }
+}
+
---
+++ PDF2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+
+namespace PDF2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        //dataGridViewにファイルをドロップしたときの処理
+        private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        {
+            int cnt = dataGridView1.Rows.Count;//行追加前の全行数
+            string[] filename = (string[])e.Data.GetData(DataFormats.FileDrop, false);                          //ドロップしたファイルのパスを配列にする
+            foreach (string str in filename)
+            {                
+                if (System.IO.Path.GetExtension(str) == ".pdf" || System.IO.Path.GetExtension(str) == ".PDF")   //PDFファイルだけdataGridViewに追加する
+                {
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[cnt].Cells[0].Value = System.IO.Path.GetFileName(str);                   //ファイル名
+                    dataGridView1.Rows[cnt].Cells[1].Value = str;                                               //フルパス
+                    cnt++;
+                }
+            }
+        }
+
+        private void dataGridView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }                
+        }
+
+        //「上へ」ボタン
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rowind = dataGridView1.SelectedCells[0].RowIndex;       //選択した行のIndex
+            int rowcnt = dataGridView1.Rows.Count;                      //全行数
+
+            if (rowind == 0) return;                                    //一番上の行のときは何もしない
+
+            DataGridViewRow dgvr = dataGridView1.Rows[rowind];
+            dataGridView1.Rows.RemoveAt(rowind);
+
+            if(rowcnt == rowind)
+            {
+                dataGridView1.Rows.Insert(rowind, dgvr);                //一番下の行だった時だけズレるので
+            }
+            else
+            {
+                dataGridView1.Rows.Insert(rowind - 1, dgvr);
+            }
+            dataGridView1.Rows[rowind - 1].Cells[0].Selected = true;    //挿入した行を選択する
+        }
+
+
+        //「下へ」ボタン
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int rowind = dataGridView1.SelectedCells[0].RowIndex;   //選択した行のIndex
+            int rowcnt = dataGridView1.Rows.Count;                  //全行数
+
+            if (rowind+1 == rowcnt) return;                         //一番下の行のときは何もしない
+
+            DataGridViewRow dgvr = dataGridView1.Rows[rowind];      //選択した行のコピーをとっておく
+            dataGridView1.Rows.RemoveAt(rowind);                    //選択した行を消す
+            dataGridView1.Rows.Insert(rowind + 1, dgvr);            //コピーしておいた行を挿入
+            dataGridView1.Rows[rowind + 1].Cells[0].Selected = true;//挿入した行を選択する（これがないと連続で移動するとき不便）
+        }
+
+
+        //「結合」ボタン
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //結合するPDFファイルのパスのリストを作成
+            List<string> path_list = new List<string>();
+            foreach (object row in dataGridView1.Rows)
+            {
+                DataGridViewRow hoge = (DataGridViewRow)row;
+                path_list.Add(hoge.Cells[1].Value.ToString());
+            }
+
+            //保存先を選択して､結合する
+            //https://dobon.net/vb/dotnet/form/savefiledialog.html
+            //https://zero0nine.com/archives/2866
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "PDFファイル(*.PDF)|*.PDF";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(dialog.FileName, FileMode.Create);   //保存先のFileStreamを取得する
+                Document doc = new Document();                                      //Documentのインスタンス
+                PdfCopy pdfcpy = new PdfCopy(doc, fs);                              //PdfCopyのインスタンス：pdfcopyの中のdoc(文書本体？)にPDFファイルを継ぎ足していく
+                doc.Open();                                                         //OpenメソッドでDocumentを開いて内容編集可能にする
+                foreach (string str in path_list)                                   //結合するPDFファイル分だけループを回す
+                {
+                    PdfReader reader = new PdfReader(str);                          //PDFReader:外部のPDFファイルを読み込む
+                    pdfcpy.AddDocument(reader);                                     //読み込んだPDFをpdfcopyに追加する
+                    reader.Close();                                                 //readerは都度閉じる
+                }
+                pdfcpy.Close();                                                     //closeメソッドで､Filestreamがflushされて結合されたPDFファイルが完成
+            }
+        }   
+    }
+}
